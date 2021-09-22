6a8e258b18622bfef3fc047dab266079127829e4
JeongMi Jo <jungim7490@gmail.com>
2021-09-22 15:26:56 +0900

    error

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import App from './src/App';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+export default App;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+/*
+리액트 네비게이션 설치
+npm install @react-navigation/native
+
+리액트 네비게이션 라이브러리 사용에 필요한 추가 라이브러리 설치
+expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
+
+추가로 필요한 라이브러리 설치(스택 네비게이션과 탭 네비게이션 활용을 위한)
+npm install @react-navigation/stack @react-navigation/bottom-tabs
+
+스타일드 컴포넌트 라이브러리 설치
+npm install --save --legacy-peer-deps styled-components
+
+prop-types 라이브러리 설치
+npm install prop-types
+
+파이어베이스 라이브러리 설치
+expo install firebase
+
+npm install react-native-keyboard-aware-scroll-view
+
+expo install expo-app-loading
+*/
