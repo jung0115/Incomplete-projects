f4475a99a52e5ac6fe4e0343831203490fd83eeb
jung0115 <jungim7490@gmail.com>
2022-03-28 21:42:16 +0900

    login 컴포넌트 배치

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,13 @@
 //로그인 페이지
 import React from 'react';
 import styled from 'styled-components/native';
-import { Text, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { Svg, Line } from 'react-native-svg';
 import * as Font from 'expo-font';
 import Loading from './Loading';
 
 // 전체
-const Container = styled.View`
+const Container = styled.SafeAreaView`
   flex: 1;
   background-color: ${({ theme }) => theme.background};
 `;
@@ -20,34 +20,39 @@ const LogoView = styled.View`
     flex: 3;
 `;
 // 이메일 박스 공간
-const EmailView = styled.View`
+const EmailView = styled.TouchableOpacity`
     flex: 1;
     background-color: ${({ theme }) => theme.loginTextBox};
     justify-content: center;
     margin: 1% 14.5% 1% 14.5%;
 `;
 // 비번 박스 공간
-const PasswordView = styled.View`
+const PasswordView = styled.TouchableOpacity`
     flex: 1;
     background-color: ${({ theme }) => theme.loginTextBox};
     justify-content: center;
     margin: 1% 14.5% 1% 14.5%;
 `;
 // 로그인 버튼 공간
-const LoginView = styled.View`
-    flex: 1.5;
-    justify-content: center;
+const LoginView = styled.TouchableOpacity`
+    flex: 1.3;
+    justify-content: flex-end;
 `;
 // 이메일, 비번 찾기 공간
 const SearchView = styled.View`
-    flex: 1.5;
-    align-items: center;
+    flex: 1.7;
+    align-items: flex-start;
+    justify-content: center;
+    display: flex;
+    flex-direction: row;
 `;
 // 가입문구 공간
 const SignupView = styled.View`
     flex: 2;
     align-items: center;
     justify-content: center;
+    display: flex;
+    flex-direction: row;
 `;
 
 // 로고
@@ -57,30 +62,28 @@ const Logo = styled.Image`
     margin: 0 0 0 15%;
 `;
 // 환영 문구
-const WelcomeTextFront = styled.Text`
-    font-size: 24px;
-    font-family: 'NotoSansCJKkr-Bold';
-    margin: 0 0 0 14%;
-`;
-const WelcomeTextBack = styled.Text`
-    font-size: 24px;
-    font-family: 'SCDream6';
-`;
-const Welcome = styled.View`
-    display: flex;
-    flex-direction: row;
+const Welcome = styled.Image`
+    width: 109.44px;
+    height: 20.48px;
+    margin: 2% 0 0 15%;
 `;
 // 이메일, 비번 텍스트
 const InputTitle = styled.Text`
-    font-size: 12px;
+    font-size: 11px;
     font-family: 'NotoSansCJKkr-Medium';
-    margin: 1% 0 1% 5%;
+    margin: 1% 0 0 5%;
+    color: ${({ theme }) => theme.mainBlack};
 `;
 // 이메일, 비번 입력 텍스트
+const InputLogin = styled.TextInput`
+    height: 14px;
+    padding: 0;
+    margin: 0 0 3% 5%;
+`;
 // 로그인 버튼
 const LoginButton = styled.TouchableOpacity`
-    background-color: ${({ theme }) => theme.loginButton};
-    margin: 1% 14.5% 1% 14.5%;
+    background-color: ${({ theme }) => theme.mainBlack};
+    margin: 1% 14.5% 0% 14.5%;
     padding: 1% 0 1% 0
     align-items: center;
 `;
@@ -91,14 +94,31 @@ const LoginText = styled.Text`
     color: white;
 `;
 // 이메일, 비번 찾기 text
+const SearchText = styled.Text`
+    font-size: 12px;
+    font-family: 'NotoSansCJKkr-Bold';
+    color: ${({ theme }) => theme.searchText};
+`;
 // 가입 문구
+const SignupTextFront = styled.Text`
+    font-size: 10px;
+    font-family: 'NotoSansCJKkr-Light';
+`;
+const SignupTextBack = styled.Text`
+    font-size: 11px;
+    font-family: 'NotoSansCJKkr-Medium';
+    color: ${({ theme }) => theme.mainBlack};
+`;
 
 class Login extends React.Component {
     constructor(props) {
         super(props);
         
-        // 폰트로딩이 완료되면 true로 변경
-        this.state = {isReady: false};
+        this.state = {
+            isReady: false, // 폰트로딩이 완료되면 true로 변경
+            isVisibleEmailInput: false, //이메일 박스 클릭하면 입력창 나타남
+            isVisiblePasswordInput: false, //비번 박스 클릭하면 입력창 나타남
+        };
     }
     
     async componentDidMount() {
@@ -112,6 +132,34 @@ class Login extends React.Component {
         // 폰트로드가 완료되어 true로 변경
         this.setState({ isReady: true });
     }
+
+    //이메일 박스 클릭 시 입력 창 나타났다 사라졌다 하는 기능
+    renderEmailInput() {
+        if(this.state.isVisibleEmailInput) {
+            return (
+                <InputLogin/>
+            );
+        }
+        return null;
+    };
+
+    emailVisible(){
+        this.setState({ isVisibleEmailInput: !this.state.isVisibleEmailInput });
+    };
+
+    //비번 박스 클릭 시 입력 창 나타났다 사라졌다 하는 기능
+    renderPasswordInput() {
+        if(this.state.isVisiblePasswordInput) {
+            return (
+                <InputLogin/>
+            );
+        }
+        return null;
+    };
+
+    passwordVisible(){
+        this.setState({ isVisiblePasswordInput: !this.state.isVisiblePasswordInput });
+    };
 
     render () {
         if (this.state.isReady) {
@@ -127,19 +175,18 @@ class Login extends React.Component {
                                 x2="32%"
                                 y2="0%"
                                 stroke="black"
-                                strokeWidth="5"
+                                strokeWidth="3"
                             />
                         </Svg>
-                        <Welcome>
-                            <WelcomeTextFront>어서오세요 </WelcomeTextFront>
-                            <WelcomeTextBack>:)</WelcomeTextBack>
-                        </Welcome>
+                        <Welcome source={require("../assets/Images/login-welcome.png")}/>
                     </LogoView>
-                    <EmailView style={styles.box}>
+                    <EmailView style={styles.box} activeOpacity={1} onPress={() => this.emailVisible()}>
                         <InputTitle>이메일</InputTitle>
+                        {this.renderEmailInput()}
                     </EmailView>
-                    <PasswordView style={styles.box}>
+                    <PasswordView style={styles.box} activeOpacity={1} onPress={() => this.passwordVisible()}>
                         <InputTitle>비밀번호</InputTitle>
+                        {this.renderPasswordInput()}
                     </PasswordView>
                     <LoginView>
                         <LoginButton style={styles.button}>
@@ -147,10 +194,13 @@ class Login extends React.Component {
                         </LoginButton>
                     </LoginView>
                     <SearchView>
-                        <Text>아이디 찾기 | 비밀번호 찾기</Text>
+                        <SearchText>아이디 찾기</SearchText>
+                        <SearchText>   |   </SearchText>
+                        <SearchText>비밀번호 찾기</SearchText>
                     </SearchView>
                     <SignupView>
-                        <Text>아직 회원이 아니신가요? 지금 바로 가입 할래요.</Text>
+                        <SignupTextFront>아직 회원이 아니신가요? </SignupTextFront>
+                        <SignupTextBack>지금 바로 가입 할래요.</SignupTextBack>
                     </SignupView>
                 </Container>
             );
--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,6 @@
 export const theme = {
     background: '#ffffff',
     loginTextBox: '#e5e5e5',
-    loginButton: '#231815'
+    mainBlack: '#231815',
+    searchText: '#a3a3a3',
 };
